09f41cb07e0962a950984924a169f73f69d58fb5
NilsVD05 <nils@von-daeniken.ch>
2020-07-03 14:24:54 +0200

    Add index.html

---
+++ jump.py
@@ -0,0 +1,55 @@
+import pygame
+pygame.init() 
+ 
+win = pygame.display.set_mode((1300,650))  
+pygame.display.set_caption("First Game") 
+
+x = 50
+y = 50
+width = 40
+height = 40
+vel = 5
+isJump = False 
+jumpCount = 10 
+run = True 
+
+ 
+while run:   
+    pygame.time.delay(20)  
+
+    for event in pygame.event.get():   
+        if event.type == pygame.QUIT:          
+            run = False
+
+    keys = pygame.key.get_pressed()
+    if keys[pygame.K_LEFT]  and x > vel:
+        x -= vel
+    if keys[pygame.K_RIGHT] and x < 1300 - vel - width: 
+        x += vel 
+
+    if not(isJump):
+        if keys[pygame.K_UP]    and y > vel: 
+            y -= vel
+        if keys[pygame.K_DOWN]  and y < 650 - height - vel: 
+            y += vel
+        if keys[pygame.K_SPACE]:    
+          isJump = True 
+        if keys[pygame.K_q]:    
+           win.fill((0,0,0)) 
+
+    if isJump:
+        if jumpCount >= -10:
+            y -= (jumpCount * abs(jumpCount)) * 0.5
+            jumpCount -= 1
+        else:
+             jumpCount = 10  
+             isJump = False   
+    
+
+    
+    pygame.draw.rect(win, (0,233,120), (x, y, width, height))  
+    
+    pygame.display.update()
+
+
+pygame.quit()
